0b3d7eea0222c96a7bd106e13ee3dadd7ab42f21
Chris Howard <iamanoptimist@gmail.com>
2013-12-02 09:48:41 -0500

    Architecture Change

---
+++ includes/inc_head.php
@@ -0,0 +1,9 @@
+<html> 
+	<head>
+        <meta charset="utf-8"/>
+	<title>My Page</title> 
+	<meta name="viewport" content="width=device-width, initial-scale=1"> 
+        <link rel="stylesheet" href="css/jquery.mobile-1.3.0.min.css" />
+        <script src="js/jquery-1.9.1.min.js"></script>
+        <script src="js/jquery.mobile-1.3.0.min.js"></script>
+</head> 
---
+++ index.php
@@ -0,0 +1,11 @@
+
+<?php
+#This include contains everything in the <head></head> tags
+include "includes/inc_head.php";
+
+#This include starts the <body> tag
+include "includes/inc_body_start.php";
+
+#This include ends the </body> tag
+include "includes/inc_body_end.php";
+?>
